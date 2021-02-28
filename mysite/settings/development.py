fb4901b1ef874628c7540a3799d7d88c2fc1fa51
TortoiseKnight <yang.liu22@outlook.com>
2021-02-28 22:16:47 +0800

    更新

--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -12,18 +12,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from .base import *
 from pathlib import Path
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(hp3#-vvk^$9=z=twk1x4lx8g31q9%2h*yhm*vubwk)vcr%&a('
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -53,6 +53,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = 'liuyang.knight@qq.com'
-EMAIL_HOST_PASSWORD = 'fyowtoucdtckbgfi'  # 授权码
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']  # 授权码
 EMAIL_SUBJECT_PREFIX = '[liuyang 的博客]'
 EMAIL_USE_TLS = True  # 与 SMTP 服务器通信时，是否启动 TLS 链接（安全链接）
